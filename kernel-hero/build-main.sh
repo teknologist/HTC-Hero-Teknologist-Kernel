37c75067b64015e375f9af750ef84d883c4dd3d6
The Teknologist <teknologist@gmail.com>
2009-11-28 14:51:58 +0100

    Removed build scripts that are bound to my systems path etc. Modified config to fix wifi tether issues with previous kernel versions

--- kernel-hero/build-main.sh
+++
@@ -1,1 +0,0 @@
-make ARCH=arm CROSS_COMPILE=/opt/Android/Build/luoAndroid/prebuilt/linux-x86/toolchain/arm-eabi-4.4.0/bin/arm-eabi-
--- kernel-hero/build-wlan.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-cd /root/1.6r1/system/wlan/ti/sta_dk_4_0_4_32
-make KERNEL_DIR=/root/kernel-hero ARCH=arm CROSS_COMPILE=~/1.6r1/prebuilt/linux-x86/toolchain/arm-eabi-4.4.0/bin/arm-eabi- clean
-make KERNEL_DIR=/root/kernel-hero ARCH=arm CROSS_COMPILE=~/1.6r1/prebuilt/linux-x86/toolchain/arm-eabi-4.4.0/bin/arm-eabi-
-
